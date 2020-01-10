575fd2565724139f3797e4cb52aa812d17387f01
Victoria Quennessen <viquennessen@gmail.com>
2020-01-10 11:01:53 -0800

    added length_at_age.R function

---
+++ R/length_at_age.R
@@ -0,0 +1,34 @@
+#' Calculates length at age
+#'
+#' @param rec_age
+#' @param max_age
+#' @param L1f
+#' @param L2f
+#' @param Kf
+#' @param a1f
+#' @param a2f
+#' @param all_ages
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' length_at_age(3, 40, 17, 49, -0.17, 1, 25, F)
+
+length_at_age = function(rec_age, max_age, L1f, L2f, Kf, a1f, a2f, all_ages = F) {
+
+  if (all_ages == T) {
+    ages <- 1:max_age } else {
+      ages <- rec_age:max_age
+    }
+
+  L_inf <- L1f + (L2f - L1f)/(1 - exp(-1*Kf*(a2f - a1f)))
+  L <- L_inf + (L1f - L_inf)*exp(-1*Kf*(ages - a1f))
+
+  return(L)
+
+}
+
+
+
+
