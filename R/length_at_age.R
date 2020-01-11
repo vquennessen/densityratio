2b90293d86aa2c12ee5a6bf1be7cd2ed728f07d4
Victoria Quennessen <viquennessen@gmail.com>
2020-01-10 20:52:37 -0800

    updated parameters in length_at_age and roxygen skeleton

--- R/length_at_age.R
+++ R/length_at_age.R
@@ -1,30 +1,31 @@
-#' Calculates length at age
+#' Length at age
 #'
-#' @param rec_age numeric value, gives age at which fish has entered the fishery
-#' @param max_age numeric value
-#' @param L1f numeric value
-#' @param L2f numeric value
-#' @param Kf numeric value
-#' @param a1f numeric value
-#' @param a2f numeric value
-#' @param all_ages logical vector, if T, gives length at ages not included in
-#' population dynamics
+#' \code{length_at_age} returns a vector of lengths at ages, in centimeters.
 #'
-#' @return A vector of lengths at ages
+#' @param Rec_age numeric, gives age at which fish has entered the fishery in years
+#' @param Max_age numeric, gives maximum age of fish or total lifespan in years
+#' @param A1 numeric, age 1 of female fish in years
+#' @param L1 numeric, gives the length of females at age 1 in cm
+#' @param A2 numeric, age 2 of female fish in years
+#' @param L2 numeric, gives the length of females at age 2 in cm
+#' @param K numeric, gives the von Bertalanffy growth parameter for females
+#' @param All_ages logical, determines if the returned vector should contain lengths for ages before recruitment
+#' @return A vector of lengths at ages, from rec_age to max_age
 #' @export
 #'
 #' @examples
-#' length_at_age(3, 40, 17, 49, -0.17, 1, 25, FALSE)
+#' length_at_age(Rec_age = 3, Max_age = 35, A1 = 5, L1 = 32.21, A2 = 15,
+#' L2 = 47.95, K = 0.2022, All_ages = FALSE)
 
-length_at_age = function(rec_age, max_age, L1f, L2f, Kf, a1f, a2f, all_ages = F) {
+length_at_age = function(Rec_age, Max_age, A1, L1, A2, L2, K, All_ages = F) {
 
-  if (all_ages == T) {
-    ages <- 1:max_age } else {
-      ages <- rec_age:max_age
+  if (All_ages == T) {
+    Ages <- 1:Max_age } else {
+      Ages <- Rec_age:Max_age
     }
 
-  L_inf <- L1f + (L2f - L1f)/(1 - exp(-1*Kf*(a2f - a1f)))
-  L <- L_inf + (L1f - L_inf)*exp(-1*Kf*(ages - a1f))
+  L_inf <- L1 + (L2 - L1)/(1 - exp(-1*K*(A2 - A1)))
+  L <- L_inf + (L1 - L_inf)*exp(-1*K*(Ages - A1))
 
   return(L)
 
