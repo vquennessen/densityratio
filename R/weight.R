50cc91737c6e7d5268a259af0bb73cf1faf9c425
Victoria Quennessen <viquennessen@gmail.com>
2020-01-14 08:39:16 -0800

    updated weight_at_age to be weight

--- R/weight.R
+++ R/weight.R
@@ -1,6 +1,6 @@
 #' Weight at age
 #'
-#' \code{weight_at_age} returns a vector of weights at age, in kg.
+#' \code{weight} returns a vector of weights at age, in kg.
 #'
 #' @param L numeric vector, gives length at age vector, in cm
 #' @param WA numeric value, coefficient in weight at length equation, where
@@ -15,8 +15,8 @@
 #' @examples
 #' L <- length_at_age(Rec_age = 2, Max_age = 35, A1 = 5, L1 = 32.21, A2 = 15,
 #' L2 = 47.95, K = 0.2022, All_ages = FALSE)
-#' weight_at_age(L, WA = 1.68e-5, WB = 3)
-weight_at_age = function(L, WA, WB) {
+#' weight(L, WA = 1.68e-5, WB = 3)
+weight = function(L, WA, WB) {
 
   # Weight at age
   # Based on Babcock & MacCall (2011): Eq. (11)
