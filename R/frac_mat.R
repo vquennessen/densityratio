dceaeded55457eda1c267911209ee3aef3e670f5
Victoria Quennessen <viquennessen@gmail.com>
2020-01-14 11:12:48 -0800

    troubleshooting frac_mat.R

--- R/frac_mat.R
+++ R/frac_mat.R
@@ -4,24 +4,23 @@
 #'    in years
 #' @param Max_age numeric value, gives maximum age of fish or total lifespan in
 #'    years
-#' @param K_mat numeric value, slope of maturity curve
+#' @param K_mat numeric value, gives slope of maturity curve
 #' @param L numeric vector, gives length at age vector, in cm
 #' @param L50 numeric value, gives length at 50% maturity, in cm
 #'
-#' @return a numeric vector of fractions mature at age, or the proportion of
-#'    individuals that are expected to be mature at any age, on the interval
-#'    (0, 1).
+#' @return
 #' @export
 #'
 #' @examples
-#' L <- length_at_age(Rec_age = 2, Max_age = 35, A1 = 5, L1 = 32.21, A2 = 15,
-#' L2 = 47.95, K = 0.2022, All_ages = FALSE)
-#' frac_mat(Rec_age = 3, Max_age = 35, K_mat = -0.4103, L, L50 = 39.53)
-
+#' L <- length_age(Rec_age = 2, Max_age = 35, A1 = 5, L1 = 32.21, A2 = 15, L2 = 47.95, K = 0.2022, All_ages = FALSE)
+#' frac_mat(Rec_age = 2, Max_age = 35, K_mat = -0.4103, L, L50 = 39.53)
 frac_mat = function(Rec_age, Max_age, K_mat, L, L50) {
 
+  # number of age classes
+  num <- length(Rec_age:Max_age)
+
   # Initialize fraction mature at age vector
-  mature <- array(rep(0, Num), c(1, Num))
+  mature <- array(rep(0, num), c(1, num))
 
   # Calculate fraction mature at age
   mature <- (1)/(1 + exp(K_mat*(L - L50)))
