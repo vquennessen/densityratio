dbfb667f4f2479914e49ff16e1ef02475295931d
Victoria Quennessen <viquennessen@gmail.com>
2020-01-14 11:12:09 -0800

    rename length_at_age to length_age in weight.R

--- R/weight.R
+++ R/weight.R
@@ -13,7 +13,7 @@
 #' @export
 #'
 #' @examples
-#' L <- length_at_age(Rec_age = 2, Max_age = 35, A1 = 5, L1 = 32.21, A2 = 15,
+#' L <- length_age(Rec_age = 2, Max_age = 35, A1 = 5, L1 = 32.21, A2 = 15,
 #' L2 = 47.95, K = 0.2022, All_ages = FALSE)
 #' weight(L, WA = 1.68e-5, WB = 3)
 weight = function(L, WA, WB) {
