6f6db247b918035d4e59503283a7c55f4579f2f2
Victoria Quennessen <viquennessen@gmail.com>
2020-02-10 10:17:32 -0800

    control rule error handling

--- R/control_rule.R
+++ R/control_rule.R
@@ -54,6 +54,71 @@ control_rule <- function(t, cr, A = 5, E, Count, Time1 = 50, TimeT = 70,
                          Outside = c(1, 2, 4, 5), Areas_sampled = 'all',
                          Ind_sampled = 'all', Years_sampled = 1) {
 
+  ###### Error handling ########################################################
+
+  # classes of variables
+  if (t %% 1 != 0) {stop('t must be an integer value.')}
+  if (cr %% 1 != 0) {stop('cr must be an integer value.')}
+  if (A %% 1 != 0) {stop('A must be an integer value.')}
+  if (!is.numeric(E)) {stop('E must be a numeric array.')}
+  if (!is.numeric(Count)) {stop('Count must be a numeric array.')}
+  if (Time1 %% 1 != 0) {stop('Time1 must be an integer value.')}
+  if (TimeT %% 1 != 0) {stop('TimeT must be an integer value.')}
+  if (Transects %% 1 != 0) {stop('Transects must be an integer value.')}
+  if (!is.numeric(Nat_mortality)) {stop('Nat_mortality must be a numeric vector.')}
+  if (!is.numeric(Final_DR)) {stop('Final_DR must be a numeric value.')}
+  if (Inside %% 1 != 0) {stop('Inside must be an integer vector.')}
+  if (Outside %% 1 != 0) {stop('Outside must be an integer vector.')}
+  if (!is.character(Areas_sampled)) {stop('Areas_sampled must be a character value.')}
+  if (!is.character(Ind_sampled)) {stop('Ind_sampled must be a character value.')}
+  if (Years_sampled %% 1 != 0) {stop('Years_sampled must be an integer value.')}
+
+  # acceptable values
+  if (t <= 0) {stop('t must be greater than 0.')}
+  if (cr <= 0) {stop('cr must be greater than 0.')}
+  if (A <= 0) {stop('A must be greater than 0.')}
+  if (sum(E < 0) > 0) {stop('All values in E must be greater than or equal to 0.')}
+  if (sum(Count < 0) > 0) {
+    stop('All values in Count must be greater than or equal to 0.')}
+  if (Time1 <= 0) {stop('Time1 must be greater than 0.')}
+  if (TimeT <= 0) {stop('TimeT must be greater than 0.')}
+  if (Transects <= 0) {stop('Transects must be greater than 0.')}
+  if (sum(Nat_mortality <= 0) > 0 || sum(Nat_mortality > 1) > 0) {
+    stop('All values in Nat_mortality must be between 0 and 1.')}
+  if (Final_DR <= 0) {stop('Final_DR must be greater than 0.')}
+  if (sum(Inside < 0) > 0) {
+    stop('All values in Inside must be greater than or equal to 0.')}
+  if (sum(Outside < 0) > 0) {
+    stop('All values in Outside must be greater than or equal to 0.')}
+  if (Areas_sampled != 'far' && Areas_sampled != 'all') {
+    stop('Areas_sampled must be either "far" or "all".')}
+  if (Ind_sampled != 'mature' && Ind_sampled != 'all') {
+    stop('Ind_sampled must be either "mature" or "all".')}
+  if (Years_sampled <= 0) {stop('Years_sampled must be greater than 0.')}
+
+  # relational values
+  if (sum(Inside > A) > 0) {
+    stop('All values in Inside must be less than or equal to A.')}
+  if (sum(Outside > A) > 0) {
+    stop('All values in Outside must be less than or equal to A.')}
+  if (sum(intersect(Inside, Outside)) > 0) {
+    stop('Areas cannot both be inside and outside the marine reserve.')}
+  if (Time1 >= TimeT) {stop('TimeT must be greater than Time1.')}
+  if(dim(E)[1] != dim(Count)[1] || dim(E)[1] != A) {
+    stop('E or Count has an incorrect number of areas.')}
+  if(dim(E)[2] != dim(Count)[2] || dim(E)[2] != TimeT) {
+    stop('E or Count has an incorrect number of time steps.')}
+  if(dim(Count)[3] != Transects) {stop('Count has the wrong number of transects.')}
+  if(dim(E)[3] != dim(Count)[5]) {
+    stop('E or Count has an incorrect number of control rules.')}
+  if(dim(E)[4] != dim(Count)[6]) {
+    stop('E or Count has an incorrect number of natural mortality estimates.')}
+  if (t > dim(E)[2]) {stop('The given "t" value is too high for E.')}
+  if (cr > dim(E)[3]) {stop('The given "cr" value is too high for E.')}
+  if (length(Nat_mortality) > dim(E)[5]) {
+    stop('Incorrect number of natural mortality estimates.')}
+
+  ##############################################################################
 
   if (cr < 4) { nm = cr } else { nm = cr - 3 }
 
