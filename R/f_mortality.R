31d8aa8dc674323ff1ef9abe5d18c813f3b2f5e7
Victoria Quennessen <viquennessen@gmail.com>
2020-02-10 11:33:42 -0800

    error handling for f_mortality.R

--- R/f_mortality.R
+++ R/f_mortality.R
@@ -37,6 +37,51 @@
 #' f_mortality(a = 1, t = 1, cr = 1, nm = 1, FM, A = 5, Fb = 0.2, E, S)
 f_mortality <- function(a, t, cr, nm, FM, A, Fb, E, S) {
 
+  ###### Error handling ########################################################
+
+  # classes of variables
+  if (a %% 1 != 0) {stop('a must be an integer value.')}
+  if (t %% 1 != 0) {stop('t must be an integer value.')}
+  if (cr %% 1 != 0) {stop('cr must be an integer value.')}
+  if (nm %% 1 != 0) {stop('nm must be an integer value.')}
+  if (!is.numeric(FM)) {stop('FM must be a numeric array.')}
+  if (A %% 1 != 0) {stop('A must be an integer value.')}
+  if (!is.numeric(Fb)) {stop('Fb must be a numeric value.')}
+  if (!is.numeric(E)) {stop('E must be a numeric array.')}
+  if (!is.numeric(S)) {stop('S must be a numeric vector.')}
+
+  # acceptable values
+  if (a <= 0) {stop('a must be greater than 0.')}
+  if (t <= 0) {stop('t must be greater than 0.')}
+  if (cr <= 0) {stop('cr must be greater than 0.')}
+  if (nm <= 0) {stop('nm must be greater than 0.')}
+  if (sum(FM < 0) > 0 || sum(FM > 1) > 0) {
+    stop('All values in FM must be between 0 and 1.')}
+  if (A <= 0) {stop('A must be greater than 0.')}
+  if (Fb < 0) {stop('Fb must be greater than or equal to 0.')}
+  if (sum(E < 0) > 0) {stop('All values in E must be greater than or equal to 0.')}
+  if (sum(S < 0) > 0 || sum(S > 1) > 0) {
+    stop('All values in S must be between 0 and 1.')}
+
+  # relational values
+  if(dim(FM)[1] != length(S)) {
+    stop('FM or S has an incorrect number of age classes.')}
+  if(dim(FM)[2] != dim(E)[1] || dim(FM)[2] != A) {
+    stop('FM or E has an incorrect number of areas.')}
+  if(dim(FM)[3] != dim(E)[2]) {
+    stop('FM or E has an incorrect number of time steps.')}
+  if(dim(FM)[4] != dim(E)[3]) {
+    stop('FM or E has an incorrect number of control rules.')}
+  if(dim(FM)[5] != dim(E)[4]) {
+    stop('FM or E has an incorrect number of natural mortality estimates.')}
+  if (a > A) {stop('The given "a" value is too high.')}
+  if (t > dim(FM)[3]) {stop('The given "t" value is too high for FM.')}
+  if (cr > dim(FM)[4]) {stop('The given "cr" value is too high for FM.')}
+  if (nm > dim(FM)[5]) {stop('The given "nm" value is too high for FM.')}
+
+  ##############################################################################
+
+
   # Catchability (Vulnerability to fishing gear)
   # Based on Babcock & MacCall (2011): Eq. (6)
   vulnerability <- (A*Fb)/(sum(E[, 1, 1, 1]))
