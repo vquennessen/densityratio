cccd85f03c6541a0a4cac132c989dff81791d070
Victoria Quennessen <viquennessen@gmail.com>
2020-02-13 18:52:14 -0800

    true_DR error handling

--- R/true_DR.R
+++ R/true_DR.R
@@ -29,6 +29,44 @@
 true_DR <- function(t, cr, Abundance_all, Inside = c(3), Outside = c(1, 2, 4, 5),
                     Density_ratio, Time1 = 50) {
 
+  ###### Error handling ########################################################
+
+  # classes of variables
+  if (t %% 1 != 0) {stop('t must be an integer value.')}
+  if (cr %% 1 != 0) {stop('cr must be an integer value.')}
+  if (!is.numeric(Abundance_all)) {
+    stop('Abundance_all must be a numeric array.')}
+  if (sum(Inside %% 1 != 0) != 0) {stop('Inside must be a vector of integers.')}
+  if (sum(Outside %% 1 != 0) != 0) {stop('Outside must be a vector of integers.')}
+  if (!is.numeric(Density_ratio)) {
+    stop('Density_ratio must be a numeric array.')}
+  if (Time1 %% 1 != 0) {stop('Time1 must be an integer value.')}
+
+  # acceptable values
+  if (t <= 0) {stop('t must be greater than 0.')}
+  if (cr <= 0) {stop('cr must be greater than 0.')}
+  if (sum(Abundance_all < 0) > 0) {
+    stop('All values in Abundance_all must be greater than or equal to 0.')}
+  if (sum(Inside < 0) > 0) {
+    stop('All values in Inside must be greater than or equal to 0.')}
+  if (sum(Outside < 0) > 0) {
+    stop('All values in Outside must be greater than or equal to 0.')}
+  if (sum(Density_ratio < 0) > 0) {
+    stop('All values in Density_ratio must be greater than or equal to 0.')}
+  if (Time1 <= 0) {stop('Time1 must be greater than 0.')}
+
+  # relational values
+  if (sum(intersect(Inside, Outside)) > 0) {
+    stop('Areas cannot both be inside and outside the marine reserve.')}
+  if (dim(Abundance_all)[1] != length(Inside) + length(Outside)) {
+    stop('Abundance_all has the wrong number of areas.')}
+ if (t > dim(Abundance_all)[2]) {
+   stop('Abundance_all has the wrong number of time steps.')}
+  if (cr > dim(Abundance_all)[3]) {
+    stop('Abundance_all has the wrong number of control rules.')}
+
+  ##############################################################################
+
   # Density of fish outside marine reserve(s)
   Outside_density <- sum(Abundance_all[Outside, t, cr, 2]) / length(Outside)
 
