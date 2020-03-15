40cdc85458d8b2c2554448de94344e5dcd88a6f6
Victoria Quennessen <viquennessen@gmail.com>
2020-03-14 17:10:39 -0700

    edited epsilon.R to include final density ratio dimension

--- R/epsilon.R
+++ R/epsilon.R
@@ -11,6 +11,7 @@
 #'    value is 6.
 #' @param NM numeric value, the total number of estimated values of natural
 #'    mortality. Default value is 3.
+#' @param FDR numeric value, the total number of final density ratios.
 #' @param NuR numeric vector, the recruitment random normal variable, pulled
 #'    from a normal distribution of mean 0 and standard deviation equal to
 #'    Sigma_R.
@@ -22,9 +23,10 @@
 #' @export
 #'
 #' @examples
-#' NuR <- array(stats::rnorm(5*70*6*3, 0, 0.5), c(5, 70, 6, 3))
-#' epsilon(A = 5, TimeT = 70, CR = 6, NM = 3, NuR, Rho_R = 0)
-epsilon <- function (A = 5, TimeT = 70, CR = 6, NM = 3, NuR, Rho_R = 0) {
+#' A = 5; TimeT = 70; CR = 6; NM = 3; FDR = 4
+#' NuR <- array(stats::rnorm(A*TimeT*CR*NM*FDR, 0, 0.5), c(A, TimeT, CR, NM, FDR))
+#' epsilon(A, TimeT, CR, NM, FDR, NuR, Rho_R = 0)
+epsilon <- function (A = 5, TimeT = 70, CR = 6, NM = 3, FDR, NuR, Rho_R = 0) {
 
   ###### Error handling ########################################################
 
@@ -33,6 +35,7 @@ epsilon <- function (A = 5, TimeT = 70, CR = 6, NM = 3, NuR, Rho_R = 0) {
   if (TimeT %% 1 != 0) {stop('TimeT must be an integer value.')}
   if (CR %% 1 != 0) {stop('CR must be an integer value.')}
   if (NM %% 1 != 0) {stop('NM must be an integer value.')}
+  if (FDR %% 1 != 0) {stop('FDR must be an integer value.')}
   if (!is.numeric(NuR)) {stop('NuR must be a numeric array.')}
   if (!is.numeric(Rho_R)) {stop('Rho_R must be a numeric array.')}
 
@@ -41,6 +44,7 @@ epsilon <- function (A = 5, TimeT = 70, CR = 6, NM = 3, NuR, Rho_R = 0) {
   if (TimeT <= 0) {stop('TimeT must be greater than 0.')}
   if (CR < 1) {stop('CR must be greater than or equal to 1.')}
   if (NM != 1 && NM != 3) {stop('NM must be equal to 1 or 3.')}
+  if (FDR <= 0) {stop('FDR must be greater than 0.')}
   if (Rho_R < -1 || Rho_R > 1) {stop('Rho_R must be between -1 and 1.')}
 
   # relational values
@@ -49,12 +53,14 @@ epsilon <- function (A = 5, TimeT = 70, CR = 6, NM = 3, NuR, Rho_R = 0) {
   if(dim(NuR)[3] != CR) {stop('NuR has an incorrect number of control rules.')}
   if(dim(NuR)[4] != NM) {
     stop('NuR has an incorrect number of natural mortality estimates.')}
+  if(dim(NuR)[5] != FDR) {
+    stop('NuR has an incorrect number of final density ratios.')}
 
   ##############################################################################
 
   # initialize epsilon vector
   # Dimensions = area * timeT * CR * M values (3)
-  Eps <- array(rep(0, A*TimeT*CR*NM), c(A, TimeT, CR, NM))
+  Eps <- array(rep(0, A*TimeT*CR*NM*FDR), c(A, TimeT, CR, NM, FDR))
 
   # eps[, 1, ]
   Eps[, 1, , ] <- NuR[, 1, , ]*sqrt(1 + Rho_R^2)
@@ -64,8 +70,10 @@ epsilon <- function (A = 5, TimeT = 70, CR = 6, NM = 3, NuR, Rho_R = 0) {
     for (t in 2:TimeT) {
       for (cr in 1:CR) {
         for (nm in 1:NM) {
-        Eps[a, t, cr, nm] <- Rho_R*Eps[a, t-1, cr, nm] +
-          NuR[a, t, cr, nm]*sqrt(1 + Rho_R^2)
+          for (fdr in 1:FDR) {
+            Eps[a, t, cr, nm, fdr] <- Rho_R*Eps[a, t-1, cr, nm] +
+              NuR[a, t, cr, nm, fdr]*sqrt(1 + Rho_R^2)
+          }
         }
       }
     }
