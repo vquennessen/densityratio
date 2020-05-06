68aa2b11cc440b92ecfb08c8eeef04ed08359e13
Victoria Quennessen <viquennessen@gmail.com>
2020-05-06 14:46:00 -0700

    removed uncertainty in natural mortality parameters from transient_DR.R

--- R/transient_DR.R
+++ R/transient_DR.R
@@ -8,8 +8,7 @@
 #' @param TimeT numeric value, the number of years to run the model total.
 #'    Default value is 70.
 #' @param Final_DRs numeric vector, the final target density ratios.
-#' @param Nat_mortality numeric vector, the estimates of natural mortality.
-#' @param nm temporary numeric value, the current natural mortality estimate.
+#' @param M numeric value, the natural mortality on the interval (0, 1).
 #' @param fdr temporary numeric value, the current final target density ratio.
 #'
 #' @return a numeric vector, the target density ratios over all timesteps with
@@ -17,10 +16,9 @@
 #' @export
 #'
 #' @examples
-#' transient_DR(Time1 = 50, TimeT = 70, Final_DRs = c(0.2, 0.4, 0.6, 0.8, 1),
-#'    Nat_mortality = c(0.12, 0.17, 0.22), nm = 1, fdr = 1)
-transient_DR <- function(Time1 = 50, TimeT = 70, Final_DRs, Nat_mortality, nm,
-                         fdr) {
+#' transient_DR(Time1 = 50, TimeT = 70, Final_DRs = c(0.6, 0.8), M = 0.17,
+#'    fdr = 1)
+transient_DR <- function(Time1 = 50, TimeT = 70, Final_DRs, M, fdr) {
 
   ###### Error handling ########################################################
 
@@ -28,8 +26,7 @@ transient_DR <- function(Time1 = 50, TimeT = 70, Final_DRs, Nat_mortality, nm,
   if (Time1 %% 1 != 0) {stop('Time1 must be an integer value.')}
   if (TimeT %% 1 != 0) {stop('TimeT must be an integer value.')}
   if (!is.numeric(Final_DRs)) {stop('Final_DRs must be a numeric vector.')}
-  if (!is.numeric(Nat_mortality)) {stop('Nat_mortality must be a numeric vector.')}
-  if (nm %% 1 != 0) {stop('nm must be an integer value.')}
+  if (!is.numeric(M)) {stop('M must be a numeric value.')}
   if (fdr %% 1 != 0) {stop('fdr must be an integer value.')}
 
   # acceptable values
@@ -37,10 +34,7 @@ transient_DR <- function(Time1 = 50, TimeT = 70, Final_DRs, Nat_mortality, nm,
   if (TimeT <= 0) {stop('TimeT must be greater than 0.')}
   if (sum(Final_DRs <= 0) > 0) {
     stop('All values in Final_DRs must be greater than 0.')}
-  if (sum(Nat_mortality <= 0) > 0 || sum(Nat_mortality > 1) > 0) {
-    stop('All values in Nat_mortality must be between 0 and 1.')}
-  if (nm <= 0 || nm > 3) {
-    stop('nm must be greater than 0 and less than or equal to 3.')}
+  if (M <= 0 || M > 1) {stop('M must be between 0 and 1.')}
   if (fdr <= 0) {stop('fdr must be greater than 0.')}
 
   # relational values
@@ -54,7 +48,7 @@ transient_DR <- function(Time1 = 50, TimeT = 70, Final_DRs, Nat_mortality, nm,
   years <- 0:(TimeT - Time1)
 
   # calculate moving DR vector
-  target_DR <- 1 - (1 - Final_DRs[fdr])*(1 - exp(-1 * Nat_mortality[nm] * years))
+  target_DR <- 1 - (1 - Final_DRs[fdr])*(1 - exp(-1 * M * years))
 
   return(target_DR)
 
