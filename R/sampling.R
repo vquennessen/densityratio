d6590ce347f1aab389da22039d8655d2a0e0e2c8
Victoria Quennessen <viquennessen@gmail.com>
2020-05-06 14:45:42 -0700

    removed uncertainty in natural mortality parameters from sampling.R

--- R/sampling.R
+++ R/sampling.R
@@ -5,7 +5,6 @@
 #'
 #' @param t temporary numeric value, the current time step.
 #' @param cr temporary numeric value, the current control rule.
-#' @param nm temporary numeric value, the current natural mortality estimate.
 #' @param fdr temporary numeric value, the current final target density ratio.
 #' @param Delta numeric value, the proportion of positive transects divided by
 #'    depletion, also known as the constant of proportionality
@@ -40,15 +39,14 @@
 #' @importFrom stats rbinom
 #'
 #' @examples
-#' n = 34; A = 5; TimeT = 70; CR = 6; NM = 3; FDR = 4; Transects = 24
-#' Abundance <- array(rep(340, A*TimeT*CR*NM*FDR*1),
-#'    c(A, TimeT, CR, NM, FDR, 1))
-#' Count <- array(rep(50, A*TimeT*Transects*2*CR*NM*FDR),
-#'    c(A, TimeT, Transects, 2, CR, NM, FDR))
-#' NuS <- array(rnorm(A*TimeT*CR*NM*FDR*1, 0, 0.89), c(A, TimeT, CR, NM, FDR, 1))
-#' sampling(t = 51, cr = 1, nm = 1, fdr = 1, Delta = 1.6, Gamma = 31.6,
-#'    Abundance, Transects, X = 15.42, Count, NuS, A, Ind_sampled = 'all')
-sampling <- function(t, cr, nm, fdr, Delta, Gamma, Abundance, Transects = 24,
+#' n = 34; A = 5; TimeT = 70; CR = 6; FDR = 4; Transects = 24
+#' Abundance <- array(rep(340, A*TimeT*CR*FDR*1), c(A, TimeT, CR, FDR, 1))
+#' Count <- array(rep(50, A*TimeT*Transects*2*CR*FDR),
+#'    c(A, TimeT, Transects, 2, CR, FDR))
+#' NuS <- array(rnorm(A*TimeT*CR*FDR*1, 0, 0.89), c(A, TimeT, CR, FDR, 1))
+#' sampling(t = 51, cr = 1, fdr = 1, Delta = 1.6, Gamma = 31.6, Abundance,
+#'    Transects, X = 15.42, Count, NuS, A, Ind_sampled = 'all')
+sampling <- function(t, cr, fdr, Delta, Gamma, Abundance, Transects = 24,
                      X, Count, NuS, A = 5, Ind_sampled = 'all') {
 
   ###### Error handling ########################################################
@@ -56,7 +54,6 @@ sampling <- function(t, cr, nm, fdr, Delta, Gamma, Abundance, Transects = 24,
   # classes of variables
   if (t %% 1 != 0) {stop('t must be an integer value.')}
   if (cr %% 1 != 0) {stop('cr must be an integer value.')}
-  if (nm %% 1 != 0) {stop('nm must be an integer value.')}
   if (fdr %% 1 != 0) {stop('fdr must be an integer value.')}
   if (!is.numeric(Delta)) {stop('Delta must be a numeric value.')}
   if (!is.numeric(Gamma)) {stop('Gamma must be a numeric value.')}
@@ -72,8 +69,6 @@ sampling <- function(t, cr, nm, fdr, Delta, Gamma, Abundance, Transects = 24,
   # acceptable values
   if (t <= 0) {stop('t must be greater than 0.')}
   if (cr <= 0) {stop('cr must be greater than 0.')}
-  if (nm <= 0 || nm > 3) {
-    stop('nm must be greater than 0 and less than or equal to 3.')}
   if (fdr <= 0) {stop('fdr must be greater than 0.')}
   if (Delta <= 0) {stop('Delta must be greater than 0.')}
   if (Gamma <= 0) {stop('Gamma must be greater than 0.')}
@@ -95,16 +90,14 @@ sampling <- function(t, cr, nm, fdr, Delta, Gamma, Abundance, Transects = 24,
     stop('The given "t" value is too high for Abundance or Count.')}
   if (cr > dim(Abundance)[3]|| cr > dim(Count)[5]) {
     stop('The given "cr" value is too high for Abundance or Count.')}
-  if (nm > dim(Abundance)[4] || nm > dim(Count)[6]) {
-    stop('The given "nm" value is too high for Abundance or Count.')}
-  if (fdr > dim(Abundance)[5] || fdr > dim(Count)[7]) {
+  if (fdr > dim(Abundance)[4] || fdr > dim(Count)[7]) {
     stop('The given "fdr" value is too high for Abundance or Count.')}
 
   ##############################################################################
 
   # Calculate probability of detection based on odds ratio
   # Based on Babcock & MacCall (2011): Eq. (12)
-  A_all <- Abundance[, t - 1, cr, nm, fdr, 1]
+  A_all <- Abundance[, t - 1, cr, fdr, 1]
   total_all <- sum(A_all)
   odds_all <-  (Delta * A_all) / (total_all / A)
   p_all <- 1 / (1 + exp(odds_all))
@@ -116,14 +109,14 @@ sampling <- function(t, cr, nm, fdr, Delta, Gamma, Abundance, Transects = 24,
   if (sum(presence_all) == 0) {r <- sample(1:Transects, 1); presence_all[r] = 1}
 
   # Calculate species count given transects with positive visuals
-  nus <- NuS[, t - 1, cr, nm, fdr, 1]
-  All <- Gamma*Abundance[, t - 1, cr, nm, fdr, 1]*exp(nus)
-  Count[, t, , 1, cr, nm, fdr] <- presence_all %*% All
+  nus <- NuS[, t - 1, cr, fdr, 1]
+  All <- Gamma*Abundance[, t - 1, cr, fdr, 1]*exp(nus)
+  Count[, t, , 1, cr, fdr] <- presence_all %*% All
 
   if (Ind_sampled == 'mature' || is.null(Ind_sampled)) {
 
     # Calculate probability of detection based on odds ratio
-    A_mature <- Abundance[, t - 1, cr, nm, fdr, 2]
+    A_mature <- Abundance[, t - 1, cr, fdr, 2]
     total_mature <- sum(A_mature)
     odds_mature <-  (Delta * A_mature) / (total_mature / A)
     p_mature <- 1 / (1 + exp(odds_mature))
@@ -135,12 +128,12 @@ sampling <- function(t, cr, nm, fdr, Delta, Gamma, Abundance, Transects = 24,
       r <- sample(1:Transects, 1); presence_mature[r] = 1}
 
     # Calculate species count given transects with positive visuals
-    nus <- NuS[, t - 1, cr, nm, fdr, 2]
+    nus <- NuS[, t - 1, cr, fdr, 2]
     Mature <- Gamma*A_mature*exp(nus)
-    Count[, t, , 2, cr, nm, fdr] <- presence_mature %*% Mature
+    Count[, t, , 2, cr, fdr] <- presence_mature %*% Mature
 
   }
 
-  return(Count[, t, , , cr, nm, fdr])
+  return(Count[, t, , , cr, fdr])
 
 }
